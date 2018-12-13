84113cad02f84f372f474842c24a39f4e6826ef1
hamzakhanvit <hamzakhanvit@gmail.com>
2018-12-13 10:14:39 -0500

    Add script for gfa parser

--- dga/gfa_parser.py
+++ dga/gfa_parser.py
@@ -7,7 +7,30 @@ class gfa_parser(object):
          
      
      def read_gfa_file(self):
+         '''
+         Reads a GFA file
+         '''
          g = gfapy.Gfa.from_file(self.filename)
-         for line in g.lines: 
-             print(line)
+
+         #Print all lines in the GFA file
+         #print ("\tAll lines:\n===================\n")
+         #for line in g.lines: 
+             #print(line)
+
+         #print ("\tAll segments:\n===================\n")
+         #Print all segments in the GFA file
+         #for line in g.segments:
+         #    print(line)
+         
+
+         #print ("\tAll fragments:\n===================\n")
+         #Print all segments in the GFA file
+         #for line in g.fragments:
+         #    print(line)
+
+         print ("\tAll edges:\n===================\n")
+         #Print all edges in the GFA file
+         print(g.edges) 
+
          return 1          
+    
